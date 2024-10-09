df9b8cf905ceb68b5cdecb6457b2151aad61a021
PerikK <perik76@gmail.com>
2024-10-09 15:33:43 +0300

    better responsiveness -some more styling

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,14 +2,20 @@ export default function Item({ item, onDeleteItem, onToggleInCart }) {
 
 	return (
 		<>
-			<li className=''>
+			<li className={`size-fit bg-slate-700 border-2 p-3 rounded-lg justify-middle ${item.inCart ? 'bg-gray-800' : ''}`}>
 				<span
-					className={`text-2xl mr-4 ${item.inCart ? 'line-through decoration-red-500 text-slate-500' : ''}`}
+					className={`text-lg md:text-2xl mr-4 ${
+						item.inCart
+							? 'line-through decoration-red-500 text-slate-500'
+							: ''
+					}`}
 				>
 					{item.qty}. {item.itemName}
 				</span>
-				<button className='bg-slate-600 hover:text-xl p-2 rounded-lg'
-					onClick={() => onToggleInCart(item.id)}>
+				<button
+					className='bg-slate-600 hover:text-xl p-2 rounded-lg'
+					onClick={() => onToggleInCart(item.id)}
+				>
 					In 🛒
 				</button>
 				<button
